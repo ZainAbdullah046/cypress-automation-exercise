129972cb2478b8704b786f3878e010cebe9d368e
ZainAbdullah046 <zainabdullah222046@gmail.com>
2026-08-26 03:40:02 +0500

    API 12: DELETE METHOD To Delete User Account

---
+++ cypress/e2e/api/deleteAccount.cy.js
@@ -0,0 +1,24 @@
+describe('API 12: DELETE METHOD To Delete User Account', () => {
+  it('deletes the user account via API', () => {
+    cy.fixture('users').then((data) => {
+      const user = data.newUser // same account created in API 11
+
+      cy.request({
+        method: 'DELETE',
+        url: '/api/deleteAccount',
+        form: true,
+        body: {
+          email: user.email,
+          password: user.password
+        },
+        failOnStatusCode: false
+      }).then((response) => {
+        const body = JSON.parse(response.body)
+
+        expect(response.status).to.eq(200)
+        expect(body.responseCode).to.eq(200)
+        expect(body.message).to.eq('Account deleted!')
+      })
+    })
+  })
+})
