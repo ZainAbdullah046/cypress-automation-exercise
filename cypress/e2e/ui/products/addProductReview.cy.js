e1982488ab94c64b01ec6ee37391bd121b1f436e
ZainAbdullah046 <zainabdullah222046@gmail.com>
2026-08-29 02:52:15 +0500

    Test Case 16: Add review on product

--- cypress/e2e/ui/products/addProductReview.cy.js
+++ cypress/e2e/ui/products/addProductReview.cy.js
@@ -1,1 +1,24 @@
-// addProductReview test
+import HomePage from '../../../pages/HomePage'
+import ProductsPage from '../../../pages/ProductsPage'
+import ProductDetailPage from '../../../pages/ProductDetailPage'
+
+describe('Test Case 16: Add review on product', () => {
+  it('submits a product review successfully', () => {
+    
+    HomePage.visit()
+
+    HomePage.clickProducts()
+
+    ProductsPage.verifyAllProductsPageVisible()
+
+    ProductsPage.clickViewProductOfFirstProduct()
+
+    ProductDetailPage.verifyWriteReviewVisible()
+
+    ProductDetailPage.fillReviewForm('Zain Abdullah', 'zain222046@gmail.com', 'Great product, highly recommended!')
+
+    ProductDetailPage.clickSubmitReview()
+
+    ProductDetailPage.verifyReviewSuccessMessage()
+  })
+})
--- cypress/pages/ProductDetailPage.js
+++ cypress/pages/ProductDetailPage.js
@@ -7,6 +7,24 @@ class ProductDetailPage {
     cy.contains('Condition:').should('be.visible')
     cy.contains('Brand:').should('be.visible')
   }
+  
+  verifyWriteReviewVisible() {
+  cy.contains('Write Your Review').should('be.visible')
+}
+
+fillReviewForm(name, email, review) {
+  cy.get('#name').type(name)
+  cy.get('#email').type(email)
+  cy.get('#review').type(review)
+}
+
+clickSubmitReview() {
+  cy.get('#button-review').click()
+}
+
+verifyReviewSuccessMessage() {
+  cy.contains('Thank you for your review.').should('be.visible')
+}
 }
 
 export default new ProductDetailPage()
